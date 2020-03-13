b8d518b0266c2f0357891fa7204a7a0758af1f32
roselandroche <r.landroche@gmail.com>
2020-03-13 19:58:20 -0400

    Wrote tests for register

--- users/users_router.spec.js
+++ users/users_router.spec.js
@@ -10,9 +10,42 @@ beforeAll(async () => {
 
 describe("users router", () => {
   describe("register", () => {
-    it("should register new user, return 201", async () => {});
-    it("should NOT register duplicate user, return 400", async () => {});
-    it("should NOT register duplicate email, return 400", async () => {});
+    it("should register new user, return 201", async () => {
+      const res = await supertest(server)
+        .post("/register")
+        .send({
+          username: "John Smith",
+          password: "abc123",
+          email: "john@gmail.com"
+        });
+      expect(res.status).toBe(201);
+      expect(res.type).toBe("application/json");
+      expect(res.body.username).toBe("John Smith");
+    });
+    it("should NOT register duplicate user, return 400", async () => {
+      const res = await supertest(server)
+        .post("/register")
+        .send({
+          username: "John Smith",
+          password: "abc123",
+          email: "john@gmail.com"
+        });
+      expect(res.status).toBe(400);
+      expect(res.type).toBe("application/json");
+      expect(res.body.message).toBe("User already exists");
+    });
+    it("should NOT register duplicate email, return 400", async () => {
+      const res = await supertest(server)
+        .post("/register")
+        .send({
+          username: "Heather Smith",
+          password: "abc123",
+          email: "john@gmail.com"
+        });
+      expect(res.status).toBe(400);
+      expect(res.type).toBe("application/json");
+      expect(res.body.message).toBe("Email already exists");
+    });
   });
   describe("log in", () => {});
 });
