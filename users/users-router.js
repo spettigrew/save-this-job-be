31b34c2099e75f01067e982c66deed818c0b0f52
Michelle Scott <scottmm374@gmail.com>
2020-03-22 20:45:12 -0400

    Added job routes get, delete, add

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const OktaClient = require("../lib/oktaClient");
-const Users = require("./users-model");
+const userMod = require("./users-model");
 const db = require("../database/db-config");
 
 router.post("/register", async (req, res, next) => {
@@ -11,22 +11,22 @@ router.post("/register", async (req, res, next) => {
       .json({ message: "Please enter all required felids" });
 
   try {
-    const newUser = {
-      profile: {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        login: req.body.email
-      },
-      credentials: {
-        password: {
-          value: req.body.password
-        }
-      }
-    };
-    const response = await OktaClient.createUser(newUser);
-    console.log(response);
-    res.json(response);
+    // const newUser = {
+    //   profile: {
+    //     firstName: req.body.firstName,
+    //     lastName: req.body.lastName,
+    //     email: req.body.email,
+    //     login: req.body.email
+    //   },
+    //   credentials: {
+    //     password: {
+    //       value: req.body.password
+    //     }
+    //   }
+    // };
+    // const response = await OktaClient.createUser(newUser);
+    // console.log(response);
+    // res.json(response);
 
     // Only Add that user to the applications database if the user was successfully added to okta's database
     if (response.status === "ACTIVE") {
@@ -35,7 +35,7 @@ router.post("/register", async (req, res, next) => {
         lastName: req.body.lastName,
         email: req.body.email
       };
-      await Users.add(appUser, "id");
+      await userMod.add(appUser, "id");
     }
   } catch (err) {
     console.log(err);
@@ -43,12 +43,35 @@ router.post("/register", async (req, res, next) => {
   }
 });
 
+// Not sure, for Okta?
 router.get("/:email", async (req, res) => {
   try {
     const user = await db("users")
       .where({ email: req.params.email })
       .first();
 
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Get user by Id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await userMod.findBy(id);
+    res.status(200).json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Grab user jobs
+router.get("/:id/jobs", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const jobPosts = await jobMod.findBy(id);
     res.status(200).json(user);
   } catch (err) {
     next(err);
