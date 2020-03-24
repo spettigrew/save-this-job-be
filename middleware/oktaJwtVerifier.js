73469ea13aa9bf93eec8ea4f41bbceb62aa7de05
Kevin <kevincarr632@gmail.com>
2020-03-24 14:43:26 -0500

    added oktaJwtVerifier middleware to authenticate users

--- middleware/oktaJwtVerifier.js
+++ middleware/oktaJwtVerifier.js
@@ -1,32 +1,36 @@
-// const OktaJwtVerifier = require("@okta/jwt-verifier");
+const OktaJwtVerifier = require("@okta/jwt-verifier");
 
-// const jwtAuth = new OktaJwtVerifier({
-//   issuer: `https://${process.env.OKTA_ORG}/oauth2/default`,
-//   clientId: "0oa4246ntis4ZkuTd4x6",
-//   assertClaims: {
-//     aud: "api://default"
-//   }
-// });
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: process.env.OKTA_ORG,
+  clientId: process.env.OKTA_CLIENT_ID,
+  assertClaims: {
+    aud: "api://default"
+  }
+});
 
-// function authenticationRequired(req, res, next) {
-//   const authHeader = req.headers.authorization || "";
-//   const match = authHeader.match(/Bearer (.+)/);
+function authenticationRequired(req, res, next) {
+  const authHeader = req.headers.authorization || "";
+  const match = authHeader.match(/Bearer (.+)/);
 
-//   if (!match) {
-//     return res.status(401).end();
-//   }
+  if (!match) {
+    return res
+      .status(401)
+      .json({ message: "Please try logging in first, then try again!" });
+  }
 
-//   const accessToken = match[1];
-//   const expectedAudience = "api://default";
+  const accessToken = match[1];
+  const expectedAudience = "api://default";
 
-//   return OktaJwtVerifier.verifyAccessToken(accessToken, expectedAudience)
-//     .then(jwt => {
-//       req.jwt = jwt;
-//       next();
-//     })
-//     .catch(err => {
-//       res.status(401).send(err.message);
-//     });
-// }
+  return oktaJwtVerifier
+    .verifyAccessToken(accessToken, expectedAudience)
+    .then(jwt => {
+      req.jwt = jwt;
+      next();
+    })
+    .catch(err => {
+      res.status(401).json(err.message);
+      console.log(err);
+    });
+}
 
-// module.exports = authenticationRequired;
+module.exports = authenticationRequired;
