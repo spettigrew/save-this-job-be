c5daa9e76e8629222ad4cc20e06d8f9e9d36d004
Kevin <kevincarr632@gmail.com>
2020-04-06 08:53:43 -0500

    added catch statment to checkUser

--- middleware/checkUser.js
+++ middleware/checkUser.js
@@ -5,23 +5,26 @@ async function checkUser(req, res, next) {
   const lastName = req.jwt.claims.lastName;
   const email = req.jwt.claims.email;
   const user = await userMod.findByEmail(email);
-
-  if (user) {
-    req.userId = user.id;
-    next();
-  } else {
-    const appUser = {
-      firstName,
-      lastName,
-      email
-    };
-    const [id] = await userMod.add(appUser);
-    if (id) {
-      req.userId = id;
+  try {
+    if (user) {
+      req.userId = user.id;
       next();
     } else {
-      res.status(500).json({ message: "Failed saving user to database" });
+      const appUser = {
+        firstName,
+        lastName,
+        email
+      };
+      const [id] = await userMod.add(appUser);
+      if (id) {
+        req.userId = id;
+        next();
+      } else {
+        res.status(500).json({ message: "Failed saving user to database" });
+      }
     }
+  } catch (err) {
+    res.status(500).json(err);
   }
 }
 
