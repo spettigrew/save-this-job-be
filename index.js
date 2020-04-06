9c8c83e1ad3895240e000679663c154b459cd65f
Kevin <kevincarr632@gmail.com>
2020-04-06 08:13:52 -0500

    added staging database

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const server = require("./api/server");
 const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || "0.0.0.0";
 
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
