9263331657d865929eae12956c9d03bbed658115
roselandroche <r.landroche@gmail.com>
2020-05-08 19:58:21 -0400

    Added tasks model

---
+++ tasks/tasks_model.js
@@ -0,0 +1,35 @@
+const db = require("../database/db-config");
+
+function getTasks() {
+    return db("tasks").select();
+}
+
+function getTasksById(taskId) {
+    return db("tasks")
+        .select()
+        .where({ taskId });
+}
+
+async function addTask(newTask) {
+    const added = await db("tasks").insert( newTask );
+    return getTasksById(added);
+}
+
+async function updateTask(taskId, updates) {
+    await db("tasks").where({ taskId }).update(updates);
+    return getTasksById(taskId);
+}
+
+function deleteTask(taskId) {
+    return db("tasks")
+        .where({ taskId })
+        .del();
+}
+
+module.exports = {
+    getTasks,
+    getTasksById,
+    addTask,
+    updateTask,
+    deleteTask,
+}
